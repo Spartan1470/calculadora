48935987b1da556ffef358cf3d8fb0d10cb1dccd
Spartan1470 <92933974+Spartan1470@users.noreply.github.com>
2022-09-02 08:57:41 -0500

    Add files via upload

---
+++ Nueva carpeta/cal.js
@@ -0,0 +1,36 @@
+function suma() {
+    let vnumero1=document.getElementById('numero1').value
+    let vnumero2=document.getElementById('numero2').value
+    let vResultado=Number(vnumero1)+Number(vnumero2)
+    
+
+    document.getElementById('lbl').innerHTML=vResultado
+    lbl.innerHTML=vResultado
+}
+function suma() {
+    let vnumero1=document.getElementById('numero1').value
+    let vnumero2=document.getElementById('numero2').value
+    let vResultado=Number(vnumero1)+Number(vnumero2)
+    
+
+    document.getElementById('lbl').innerHTML=vResultado
+    lbl.innerHTML=vResultado
+}
+function suma() {
+    let vnumero1=document.getElementById('numero1').value
+    let vnumero2=document.getElementById('numero2').value
+    let vResultado=Number(vnumero1)+Number(vnumero2)
+    
+
+    document.getElementById('lbl').innerHTML=vResultado
+    lbl.innerHTML=vResultado
+}
+function suma() {
+    let vnumero1=document.getElementById('numero1').value
+    let vnumero2=document.getElementById('numero2').value
+    let vResultado=Number(vnumero1)+Number(vnumero2)
+    
+
+    document.getElementById('lbl').innerHTML=vResultado
+    lbl.innerHTML=vResultado
+}
